077bd4004d9fd238ec6bb97da94e5ec3e9b89327
Sergio016 <sergio.lopez16102003@gmail.com>
2024-12-17 09:41:46 +0100

    a

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,32 @@
-// backend/app.js
 const express = require('express');
-const bodyParser = require('body-parser');
+const client = require('prom-client');
 const app = express();
 
-const trabajadoresRoutes = require('./routes/trabajadores');  // Importa las rutas de trabajadores
+// Inicia la recolección de métricas predeterminadas
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();  // Esto obtiene métricas predeterminadas como uso de memoria, CPU, etc.
+
+// Define una ruta para exponer las métricas
+app.get('/metrics', (req, res) => {
+  console.log('Métricas solicitadas');
+  res.set('Content-Type', client.register.contentType);
+  res.end(client.register.metrics());
+});
+
+// Define tus rutas de la API
+const trabajadoresRoutes = require('./routes/trabajadores');
 const blogRoutes = require('./routes/blog');
 const localizacionRoutes = require('./routes/localizacion');
 const ofertasRoutes = require('./routes/ofertas');
 const productosRoutes = require('./routes/productos');
 
-
-app.use(bodyParser.json());
-
-
-
-app.use(trabajadoresRoutes);  // Usa las rutas para las solicitudes API
+app.use(trabajadoresRoutes);
 app.use(blogRoutes);
 app.use(localizacionRoutes);
-app.use(blogRoutes);
 app.use(ofertasRoutes);
 app.use(productosRoutes);
 
-
-// Inicializar el servidor en el puerto 3000
-app.listen(3000, () => {
+// Inicializa el servidor en el puerto 3000
+app.listen(3000, '0.0.0.0', () => {
   console.log('Servidor corriendo en http://localhost:3000');
 });
